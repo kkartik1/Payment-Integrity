f1dedc803f1d190d8d364814105eccf565976d08
Kartikeya, Kanishka <kanishka.kartikeya@optum.com>
2026-05-19 12:52:31 +0530

    Added Data

--- coverage/html_utils.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-def strip_html(text: str) -> str:
-    if not isinstance(text, str):
-        return ""
-    soup = BeautifulSoup(text, "html.parser")
-    return soup.get_text("\n").strip()
